4b2db7da2477a952e9e1f27eb9386d2e00a1e81d
Hassan Shakur <dev.hassanshakur@gmail.com>
2024-05-09 14:49:47 +0300

    image generator ui setup

--- app/(dashboard)/(routes)/code/page.tsx
+++ app/(dashboard)/(routes)/code/page.tsx
@@ -132,7 +132,7 @@ const CodeCompletion = () => {
             </div>
           )}
           {messages.length === 0 && !isLoading && (
-            <Empty label='No conversations started.' />
+            <Empty label='No code generated.' />
           )}
           <div className='flex flex-col-reverse gap-y-4'>
             {messages.map((message) => (
