26d431b9bba77c0474b4c9833b27f1f97f63197d
Hassan Shakur <dev.hassanshakur@gmail.com>
2024-05-10 09:11:39 +0300

    landing layout

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,3 +1,15 @@
-export default function Home() {
-  return <p>Hello world</p>;
-}
+import { LandingContent } from '@/components/landing-content';
+import { LandingHero } from '@/components/landing-hero';
+import { LandingNavbar } from '@/components/landing-navbar';
+
+const LandingPage = () => {
+  return (
+    <div className='h-full'>
+      <LandingNavbar />
+      <LandingHero />
+      <LandingContent />
+    </div>
+  );
+};
+
+export default LandingPage;
